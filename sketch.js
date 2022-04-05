3e27bc6a9b6aa3d9742372e72393b4b278c1704c
kpbiskup <102772494+kpbiskup@users.noreply.github.com>
2022-04-05 13:38:30 -0500

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,31 @@
+function setup() {
+  createCanvas(windowWidth, windowHeight, WEBGL);
+  background(0);
+}
+
+function draw() { 
+
+  for (let i = 0; i < 4; i++) {
+    for (let j = 0; j < 6; j++) {
+        let x = 0 + i * 2; 
+        let y = 0 + j * 2; 
+      
+  let locX = mouseX - height / 1;
+  let locY = mouseY - width / 1;
+
+  ambientLight(10);
+  directionalLight(250, 0.15, 0.15, 0.15, 0.15, 0.15);
+  pointLight(0, 4, 250, locX, locY, 250);
+
+  push();
+  translate(-width / 29, 0, 0);
+  rotateZ(frameCount * 0.02);
+  rotateX(frameCount * 0.02);
+  specularMaterial(255);
+  box(180, 180, 180);
+  pop();
+
+    }
+  }
+}
+
